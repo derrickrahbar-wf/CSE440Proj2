532f6dd1771b550676fc07e30f3b1f9e00cdc424
derrickrahbar-wf <derrickrahbar@workiva.com>
2014-09-26 15:45:50 -0700

    Added sym table data structures

--- symtab.h
+++ symtab.h
@@ -8,11 +8,20 @@
 #ifndef SYMTAB_H
 #define SYMTAB_H
 
+#define SCOPE_FV = 0;
+#define SCOPE_NFV = 1;
+
+#define ASSIGNMENT_STATEMENT = 0;
+#define STATEMENT_SEQUENCE = 1;
+#define IF_STATEMENT = 2;
+#define WHILE_STATEMENT = 3;
+#define PRINT_STATEMENT = 4;
+
 #include "shared.h"
 #include "usrdef.h"
 #include <stdlib.h>
 #include <string.h>
-
+#include "./uthash/src/uthash.h"
 
 
 /* ----------------------------------------------------------------
@@ -23,35 +32,46 @@
 
 void symtab_init();
 void symtab_print(int numOfTabs);
-struct class_sym_tab* new_class_sym_tab();
 
-struct sym_tab {
-    struct class_sym_tab *class_tab;
-    struct id_sym_tab *id_tab;
-};
-
-struct class_sym {
+struct attribute_key_t{
     char *id;
-    struct class_sym *extend;
-    struct class_block_t *cb;
-    int line_number; //to check if acutally declared or just referenced
-    int equiv_id; //unique id shared by all equiv classes -1 of not yet set
-
+    int scope;
+    struct function_declaration_t *function;
 };
 
-struct class_sym_tab {
-    struct class_sym *class_sym;
-    struct class_sym_tab *next;
-};
-
-struct id_sym {
-    char *id;
+struct attribute_table_t{
     struct type_denoter_t *type;
+    int line_number;
+    int is_function;
+    char *id; /* id, scope and function are used for the key */
+    int scope;
+    struct function_declaration_t *function;
+    UT_hash_handle hh; /* defines structure as a hashable object */
 };
 
-struct id_sym_tab {
-    struct id_sym *id;
-    struct id_sym_tab *next;
+struct statement_table_t{
+    int type;
+    int line_number;
+    struct function_declaration_t *function;
+    
+    union{
+        struct assignment_statement_t *as;
+        struct statement_sequence_t *ss;
+        struct if_statement_t *is;
+        struct while_statement_t *ws;
+        struct print_statement_t *ps;
+  }statement_data;
+
+  UT_hash_handle hh; /* defines structure as a hashable object */
+};
+
+struct class_table_t {
+    struct attribute_table_t *attribute_hash_table;
+    struct class_table_t *extend;
+    int line_number;
+    char *id;
+    struct statement_table_t *statement_hash_table;
+    UT_hash_handle hh; /* defines structure as a hashable object */
 };
 
 #endif
